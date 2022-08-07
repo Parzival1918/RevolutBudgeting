137ad4dd5ce0e0f442b04a89663f647684d71c1a
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2022-08-07 20:57:51 +0100

    Add files via upload

--- RevolutSankey.m
+++ RevolutSankey.m
@@ -41,3 +41,31 @@ dates = datetime(datesCell,'InputFormat','yyyy-MM-dd HH:mm:ss', 'Format', 'yyyy-
 d = rawdata.data;
 plot(dates(1:end-10),d(1:end-10));
 
+%descriptions
+descriptionsCell = data(:,3);
+descriptions = string(descriptionsCell);
+
+uniqueDesc = unique(descriptions);
+
+%how much spent per unique description
+
+paymentsCell = data(:,end);
+payments = zeros([length(paymentsCell),1]);
+for i = 1:length(paymentsCell)
+    payments(i) = str2double(paymentsCell{i}); 
+end
+
+infoDescriptions = zeros([length(uniqueDesc),2]);
+for i = 1:length(uniqueDesc)
+    description = uniqueDesc(i);
+    received = 0;
+    sent = 0;
+    
+    all = payments(descriptions == description);
+    received = sum(all(all>0));
+    sent = sum(all(all<0));
+    infoDescriptions(i,1) = received;
+    infoDescriptions(i,2) = sent;
+end
+
+t = table(uniqueDesc,infoDescriptions(:,1),infoDescriptions(:,2),'VariableNames',["Description","Received","Sent"]);
