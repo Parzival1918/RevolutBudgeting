5b23a67b08a71df33d47b6ea6b1d8e7e7821ba0e
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2022-08-08 08:57:50 +0100

    Add files via upload

--- RevolutSankey.m
+++ RevolutSankey.m
@@ -55,6 +55,33 @@ for i = 1:length(paymentsCell)
     payments(i) = str2double(paymentsCell{i}); 
 end
 
+%%Add higher level sections that nest the Unique descriptions, like
+%%groceries, travel, restaurants.
+groupNames = ["Groceries","Travel","Restaurants","University","Housing","Shopping","Rest"];
+groups = cell([length(groupNames),2]);
+
+groups{1,1} = groupNames(1);
+groups{2,1} = groupNames(2);
+groups{3,1} = groupNames(3);
+groups{4,1} = groupNames(4);
+groups{5,1} = groupNames(5);
+groups{6,1} = groupNames(6);
+groups{7,1} = groupNames(7);
+
+groups{1,2} = ["Sainsburys","Tesco Stores 5106","Tesco Stores 6479","Tesco Stores 6863",...
+    "Co-Op Group Food","Deans Family Butchers","Sq *connors Fruit Corner"];
+groups{2,2} = ["Ryanair","Trainline","Tfl Travel Charge","thetrainline.com"];
+groups{3,2} = ["200 Degrees","Bao Stoney","Bloo 88","Caffe Tucci","Castle Fruit And Salad Bo",...
+    "Cawa Division Ltd","Deliveroo","Edosushi","Howst","Iz *johns Van","Lunch Stop","Pret A Manger",...
+    "Sq *luckyfox","Sq *terrace Goods","Sumup  *5tara Sheffield","Ztl*chatime Sheff Limi",...
+    "Ztl*johns Van","Ztl*peddler Market","Ztl*ramenfications Lim"];
+groups{4,2} = ["To Univ Of Sheffield","Www.sheffieldstudentsu"];
+groups{5,2} = ["To Dove Properties And Estates Limited","To Unihomes"];
+groups{6,2} = ["Amazon Eu","Amazon Go","Amazon Prime","Amznmktplace","Nisbets Plc",...
+    "Nya*lane7 Ltd","Smith & Tissington","The Light Cinemas","Waterstones","amazon.co.uk","www.voxi.co.uk"];
+groups{7,2} = [];
+
+infoGroups = zeros([length(groupNames),2]);
 infoDescriptions = zeros([length(uniqueDesc),2]);
 for i = 1:length(uniqueDesc)
     description = uniqueDesc(i);
@@ -66,6 +93,20 @@ for i = 1:length(uniqueDesc)
     sent = sum(all(all<0));
     infoDescriptions(i,1) = received;
     infoDescriptions(i,2) = abs(sent);
+    
+    inSection = false;
+    for j = 1:length(groupNames)-1
+        if ismember(description,groups{j,2})
+            inSection = true;
+            
+            infoGroups(j,1) = infoGroups(j,1) + received;
+            infoGroups(j,2) = infoGroups(j,2) + abs(sent);
+        end
+    end
+    if ~inSection
+        infoGroups(end,1) = infoGroups(end,1) + received;
+        infoGroups(end,2) = infoGroups(end,2) + abs(sent);
+    end
 end
 
 t = table([uniqueDesc;"Total"],[infoDescriptions(:,1);sum(infoDescriptions(:,1))],...
@@ -86,4 +127,21 @@ for i = 1:length(textLines)
 end
 
 tableTxt = table(textLines);
-writetable(tableTxt,"SankeyText",'WriteVariableNames',false);
+writetable(tableTxt,"SankeyTextDescriptions",'WriteVariableNames',false);
+
+%Sankey diagram text of Groups (for https://sankeymatic.com/build/)
+allMoneySectionTxt = "Total";
+textLines = cell([length(groupNames),1]);
+
+for i = 1:length(groupNames)
+    if infoGroups(i,2) ~= 0
+        txt = strcat(allMoneySectionTxt," [",string(infoGroups(i,2)),"] ",groupNames(i));
+        textLines{i} = txt; 
+    elseif infoGroups(i,2) == 0 && infoGroups(i,1) ~= 0
+        txt = strcat(groupNames(i)," [",string(infoGroups(i,1)),"] ",allMoneySectionTxt);
+        textLines{i} = txt; 
+    end
+end
+
+tableTxt = table(textLines);
+writetable(tableTxt,"SankeyTextGroups",'WriteVariableNames',false);
